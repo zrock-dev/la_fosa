aa2d4ec666aa901db5aeb7b50b370ee785cf828d
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2022-08-15 15:14:34 -0400

    *png.import files cleanup + progress

--- src/dog/player.gd
+++ src/dog/player.gd
@@ -13,10 +13,10 @@ onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * 
 onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 # horizontal movement
-var is_right
-var is_left
-var duck
-var can_jump
+var is_right : bool
+var is_left : bool
+var down : bool
+var can_jump : bool
 
 # joystick signal
 var is_joystick_in_use
@@ -32,7 +32,7 @@ func _physics_process(delta):
 	position_player.y += get_gravity() * delta
 	position_player.x = get_input_position_player() * move_speed
 	
-	if (Input.is_action_just_pressed("move_up") or can_jump) and is_on_floor():
+	if can_jump and is_on_floor():
 		jump()
 	
 	position_player = move_and_slide(position_player, Vector2.UP)
@@ -55,7 +55,8 @@ func get_input_position_player() -> float:
 	if !is_joystick_in_use:
 		is_right = Input.is_action_pressed("move_rigth")
 		is_left = Input.is_action_pressed("move_left")
-		duck = Input.is_action_pressed("move_down")
+		down = Input.is_action_pressed("move_down")
+		can_jump = Input.is_action_just_pressed("move_up")
 	
 	if is_right or is_left:
 		$AnimationTree.set("parameters/movement/current", 1)
@@ -68,48 +69,43 @@ func get_input_position_player() -> float:
 	else:
 		$AnimationTree.set("parameters/movement/current", 0)
 		is_joystick_in_use = false
-		if duck:
+
+		if down && is_on_floor():
 			$AnimationTree.set("parameters/iddle/current", 1)
 		else:
 			$AnimationTree.set("parameters/iddle/current", 0)
 	
-	# fall fast
-	if not is_on_floor() && duck:
-		fall_gravity = fall_gravity * 2 
-			
+	check_fall_fast()
 	reset_movement_flags()
 	return horizontal
 	
 func reset_movement_flags():
 	is_right = false
 	is_left = false
-	duck = false
-
+	down = false
 
 func _on_CanvasLayer_move_signal(move_vector):
 	
 	is_joystick_in_use = true
 	var x_pos = move_vector.x
 	var y_pos = move_vector.y
-	var range_area = 0.4
-	var max_limit = 1
+	
+	var x_dist = 1 - get_absolute_value(x_pos)
+	var y_dist = 1 - get_absolute_value(y_pos)
+	print(y_pos," |-----| ", y_dist)
+	var max_dist_from_border = .3
 
 	# Joystick position
-	var rigth_cuadrant = in_range(x_pos, range_area, max_limit) && in_range(y_pos, -max_limit, max_limit)
-	var left_cuadrant = in_range(x_pos, -max_limit, -range_area) && in_range(y_pos, -max_limit, max_limit)
-	var up_cuadrant = in_range(x_pos, -max_limit, max_limit) && in_range(y_pos, -max_limit, -range_area) && is_on_floor()
-	var down_cuadrant = in_range(x_pos, -range_area, range_area) && in_range(y_pos, range_area, max_limit) && is_on_floor()
-	
-	if rigth_cuadrant: # rigth
-		is_right = true
+	is_right = (x_pos > 0) && (x_dist < max_dist_from_border)
+	is_left = (x_pos < 0) && (x_dist < max_dist_from_border)
 
-	if left_cuadrant: # left
-		is_left = true
-		
-	if up_cuadrant: # up
-		can_jump = true
-	if down_cuadrant: # down
-		duck = true
-		
-func in_range(number, mini, maxi) -> bool:
-	return number >= mini && number <= maxi
+	can_jump = (y_pos < 0) && (y_dist < .3) && position_player.y == 0.0
+
+	down = (y_pos > 0) && (y_dist < max_dist_from_border)
+
+func check_fall_fast():
+	if !is_on_floor() && down:
+		position_player.y = position_player.y + 100 
+
+func get_absolute_value(value):
+	return value if value > 0 else -value
