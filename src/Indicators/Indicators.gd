22479f82811bbc495363e7c510f1f6bd250e77e2
Camila Malaga <Camila.Malaga@fundacion-jala.org>
2022-08-19 17:12:13 -0400

    Implement Indicator Control

--- src/Indicators/Indicators.gd
+++ src/Indicators/Indicators.gd
@@ -1,14 +1,19 @@
-extends Node2D
+extends Control
 
+var text
+var color
 
-# Declare member variables here. Examples:
 enum MessageType {Info, Warning, Danger}
-# Called when the node enters the scene tree for the first time.
-var type = "Info"
+
 func _Message(var message, var type):
-	if type is MessageType.Info:
-		print(message + "verde")
 	if type is MessageType.Warning:
-		print(message + "amarillo")
-	if type is MessageType.Danger:
-		print(message + "rojo")
+		print("go")
+		#$Icon.texture.load_path = "res://assets/icon/alert.png"
+	$Text._set_text(message)
+
+
+func _on_Indicators_ready():
+	#_Message("WARNING", MessageType.Warning)
+	$Icon.texture.
+	$Icon.texture.load_path = "res://assets/icon/alert.png"
+	print("hola")
---
+++ src/Indicators/Text.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+func _set_text(var message):
+	$Label.text = message
