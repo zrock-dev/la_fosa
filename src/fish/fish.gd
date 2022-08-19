30524f059670fcc3fadd051f62d148a7b60594eb
Ariel <ariel.claros.60091@usalesiana.edu.bo>
2022-08-19 15:01:08 -0400

    Fish moves diagonally

--- src/fish/fish.gd
+++ src/fish/fish.gd
@@ -85,7 +85,7 @@ func _on_CanvasLayer_move_signal(move_vector):
 		
 	if in_range(x_pos, -max_limit, max_limit) && in_range(y_pos, -max_limit, -range_area): # up
 		is_up = true
-	if in_range(x_pos, -range_area, range_area) && in_range(y_pos, range_area, max_limit): # down
+	if in_range(x_pos, -max_limit, max_limit) && in_range(y_pos, range_area, max_limit): # down
 		is_down = true
 		
 func in_range(number, mini, maxi) -> bool:
