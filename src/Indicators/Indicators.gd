64d9fb2d122177875699dfeeda1ebfd0aaed1a37
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2022-08-23 20:17:29 -0400

    testing + color addition

--- src/Indicators/Indicators.gd
+++ src/Indicators/Indicators.gd
@@ -1,18 +1,41 @@
 extends Control
 
-var text
-var color
 onready var label := $HBoxContainer/Label
 onready var texture := $HBoxContainer/TextureRect
+onready var label_color = $HBoxContainer/Label.get_stylebox("normal", "")
+
+var info_image = preload("res://assets/in-game-alerts/info.png")
+var warning_image = preload("res://assets/in-game-alerts/warning.png")
+var danger_image = preload("res://assets/in-game-alerts/danger.png")
+
+var timer = 0
 
 enum MessageType {Info, Warning, Danger}
 
 func _Message(var message, var type):
 	label.text = message
 	
-	if type is MessageType.Warning:
-		texture.set_texture("res://assets/in-game-alerts/warning.png")
-	elif type is MessageType.Danger:
-		texture.set_texture("res://assets/in-game-alerts/danger.png")
-	elif type is MessageType.Info:
-		texture.set_texture("res://assets/in-game-alerts/info.png")
+	if type == MessageType.Warning:
+		texture.set_texture(warning_image)
+		label_color.border_color = Color.yellow
+	elif type == MessageType.Danger:
+		texture.set_texture(danger_image)
+		label_color.border_color = Color.red
+	elif type == MessageType.Info:
+		texture.set_texture(info_image)
+		label_color.border_color = Color.blue
+		
+	label_color.bg_color = label_color.border_color
+
+
+func _on_Timer_timeout():
+	print ("Timeout timer: ", timer)
+	if timer == 0:
+		_Message("This is a info message", MessageType.Info)
+		timer += 1
+	elif timer == 1:
+		_Message("This is a danger message", MessageType.Danger)
+		timer += 1
+	elif timer == 2:
+		_Message("This is a warning message", MessageType.Warning)
+		timer = 0
