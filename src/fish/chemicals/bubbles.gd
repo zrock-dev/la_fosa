b1f4cb490be6ff419e364933418dffe7696d8609
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2022-08-29 17:11:14 -0400

    Bubbles can interact with the player.
    ---- bubbles.gd
    - Added the methods _on_StaticBody2D_body_entered(), _on_StaticBody2D_body_exited() to monitor if the player is inside the ball or has exited it.
    - Moved the move sphere logic into another method called move_sphere() that is called if the flag can move is true.
    - Added the metho _on_Timer_timeout() to count, and after x seconds the player starts to get damage, or if the spehre explodes it dies.
    - Changed the original node type to Area2d
    --- GameFish
    - Modified the script to have a limit of how many spheres it can spawn.
    
    - Removed innecesary bubble images.
    - Updated the in-use bubble spritesheet to show the frame changes.

--- src/fish/chemicals/bubbles.gd
+++ src/fish/chemicals/bubbles.gd
@@ -1,14 +1,21 @@
-extends StaticBody2D
+extends Area2D
 
 onready var original_pos = position
-var up = true;
+var up = true
+var can_move = true
+
+var frames_count = 0
+var timeout_count = 0
+
 
 func _ready():
-	$Sprite.frame = randi()%4
 	position = Vector2(rand_range(10, 990), rand_range(10, 590))
 
-var frames_count = 0;
 func _process(_delta):
+	if can_move: 
+		move_sphere()
+
+func move_sphere():
 	if frames_count < 120:
 		if up:
 			position.y += .5
@@ -18,3 +25,25 @@ func _process(_delta):
 	else: 
 		up = !up
 		frames_count = 0
+
+
+func _on_StaticBody2D_body_entered(body):
+	if body.name == "Fish":
+		can_move = false
+		$Timer.start()
+		
+
+func _on_StaticBody2D_body_exited(body):
+	if body.name == "Fish":
+		can_move = true
+		timeout_count = 0
+		$Sprite.frame = 0
+		$Timer.stop()
+
+func _on_Timer_timeout():
+	if timeout_count <= 2:
+		timeout_count += 1
+		$Sprite.frame += 1
+	# Degradates the sphere every second
+
+
--- src/game/GameFish.gd
+++ src/game/GameFish.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 # bubbles generation
 var bubbles = preload("res://src/fish/chemicals/bubble.tscn")
+var bubbles_count = 1
 
 func _on_Timer_timeout():
 	randomize()
 	add_child(bubbles.instance())
+	if bubbles_count < 5:
+		bubbles_count += 1
+	else:
+		$Timer.stop()
+		$Timer.autostart = false
