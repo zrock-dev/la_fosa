972498770f6d0005edc2a614d9f3043d7d9cd7f2
Camila Malaga <Camila.Malaga@fundacion-jala.org>
2022-09-09 20:08:29 -0400

    last commit

--- src/game_over/game_over.gd
+++ src/game_over/game_over.gd
@@ -22,6 +22,5 @@ func _on_Try_Again_button_down():
 func _on_Credits_button_down():
 	pass # Replace with function body.
 
-
 func _on_Quit_button_down():
 	get_tree().change_scene("res://src/game/main_menu.tscn")
