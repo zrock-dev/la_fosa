d35b15a2a124b257d0d5f08af47289df1312ce5c
Camila Malaga <Camila.Malaga@fundacion-jala.org>
2022-09-30 14:38:52 -0400

    - Update Image - Change Lines and Fonts - Update Script Game - fix fish and dog player pause

--- src/game/Game.gd
+++ src/game/Game.gd
@@ -1,5 +1,8 @@
 extends Node2D
- 
+
+func _ready():
+	get_tree().paused = false
+
 func _on_Fish_player_deaded():
 	load_game_over( "res://src/game/GameFish.tscn")
 
@@ -7,9 +10,6 @@ func _on_Player_player_deaded():
 	load_game_over("res://src/game/Game.tscn")
 
 func load_game_over(path_to_scene):
-	print("dead", path_to_scene)
 	var game_over = preload("res://src/game_over/game_over.tscn").instance()
 	game_over.set_before_scene(path_to_scene)
 	add_child(game_over)
-
-
